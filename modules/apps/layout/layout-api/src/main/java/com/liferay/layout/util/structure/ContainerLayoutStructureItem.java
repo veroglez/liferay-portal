dd64c7d656877cca43cf9db3cba9cc67686f1d61
Victor Galan <victor.galan@liferay.com>
2020-02-12 09:13:16 +0100

    Revert "LPS-108355 Creates the JSONObject to prevent null pointer"
    This reverts commit f0593f55444e2143c0c72e622ac58e30c9d895c5.

--- modules/apps/layout/layout-api/src/main/java/com/liferay/layout/util/structure/ContainerLayoutStructureItem.java
+++ modules/apps/layout/layout-api/src/main/java/com/liferay/layout/util/structure/ContainerLayoutStructureItem.java
@@ -15,7 +15,6 @@
 package com.liferay.layout.util.structure;
 
 import com.liferay.layout.util.constants.LayoutDataItemTypeConstants;
-import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.json.JSONUtil;
 
@@ -26,8 +25,6 @@ public class ContainerLayoutStructureItem extends LayoutStructureItem {
 
 	public ContainerLayoutStructureItem(String parentItemId) {
 		super(parentItemId);
-
-		_backgroundImageJSONObject = JSONFactoryUtil.createJSONObject();
 	}
 
 	public String getBackgroundColorCssClass() {
