2ae299f533fd4019f836e06385b34eba5579879b
Victor Galan <victor.galan@liferay.com>
2021-04-12 11:58:10 +0200

    LPS-129685 Create contents contributor for AssetListEntryUsages

---
+++ modules/apps/asset/asset-list-web/src/main/java/com/liferay/asset/list/web/internal/layout/contents/contributor/AssetListEntryUsagesLayoutContentsContributor.java
@@ -0,0 +1,213 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.list.web.internal.layout.contents.contributor;
+
+import com.liferay.asset.kernel.AssetRendererFactoryRegistryUtil;
+import com.liferay.asset.kernel.model.AssetRendererFactory;
+import com.liferay.asset.kernel.model.ClassType;
+import com.liferay.asset.kernel.model.ClassTypeReader;
+import com.liferay.asset.list.model.AssetListEntry;
+import com.liferay.asset.list.model.AssetListEntryUsage;
+import com.liferay.asset.list.service.AssetListEntryLocalService;
+import com.liferay.asset.list.service.AssetListEntryUsageLocalService;
+import com.liferay.info.list.provider.InfoListProvider;
+import com.liferay.info.list.provider.InfoListProviderTracker;
+import com.liferay.layout.contents.contributor.LayoutContentsContributor;
+import com.liferay.petra.reflect.GenericUtil;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.security.permission.ResourceActionsUtil;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Locale;
+import java.util.Objects;
+import java.util.Set;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Víctor Galán
+ */
+@Component(service = LayoutContentsContributor.class)
+public class AssetListEntryUsagesLayoutContentsContributor
+	implements LayoutContentsContributor {
+
+	@Override
+	public JSONArray getLayoutContentsJSONArray(
+		HttpServletRequest httpServletRequest, long plid) {
+
+		List<AssetListEntryUsage> assetListEntryUsages =
+			_assetListEntryUsageLocalService.getAssetEntryListUsagesByPlid(
+				plid);
+
+		JSONArray mappedContentsJSONArray = JSONFactoryUtil.createJSONArray();
+
+		Set<String> uniqueAssetListEntryUsagesKeys = new HashSet<>();
+
+		for (AssetListEntryUsage assetListEntryUsage : assetListEntryUsages) {
+			if (!uniqueAssetListEntryUsagesKeys.contains(
+					_generateUniqueLayoutClassedModelUsageKey(
+						assetListEntryUsage))) {
+
+				mappedContentsJSONArray.put(
+					_getMappedContentJSONObject(
+						assetListEntryUsage, httpServletRequest));
+			}
+
+			uniqueAssetListEntryUsagesKeys.add(
+				_generateUniqueLayoutClassedModelUsageKey(assetListEntryUsage));
+		}
+
+		return mappedContentsJSONArray;
+	}
+
+	private String _generateUniqueLayoutClassedModelUsageKey(
+		AssetListEntryUsage assetListEntryUsage) {
+
+		return assetListEntryUsage.getClassNameId() + StringPool.DASH +
+			assetListEntryUsage.getKey();
+	}
+
+	private String _getAssetEntryListSubtypeLabel(
+		AssetListEntry assetListEntry, Locale locale) {
+
+		String typeLabel = ResourceActionsUtil.getModelResource(
+			locale, assetListEntry.getAssetEntryType());
+
+		String subtypeLabel = _getSubtypeLabel(assetListEntry, locale);
+
+		if (Validator.isNull(subtypeLabel)) {
+			return typeLabel;
+		}
+
+		return typeLabel + " - " + subtypeLabel;
+	}
+
+	private JSONObject _getMappedContentJSONObject(
+		AssetListEntryUsage assetListEntryUsage,
+		HttpServletRequest httpServletRequest) {
+
+		ThemeDisplay themeDisplay =
+			(ThemeDisplay)httpServletRequest.getAttribute(
+				WebKeys.THEME_DISPLAY);
+
+		JSONObject mappedContentJSONObject = JSONUtil.put(
+			"className", assetListEntryUsage.getClassName()
+		).put(
+			"classNameId", assetListEntryUsage.getClassNameId()
+		).put(
+			"classPK", assetListEntryUsage.getKey()
+		).put(
+			"type", LanguageUtil.get(httpServletRequest, "collection")
+		);
+
+		if (Objects.equals(
+				assetListEntryUsage.getClassName(),
+				AssetListEntry.class.getName())) {
+
+			AssetListEntry assetListEntry =
+				_assetListEntryLocalService.fetchAssetListEntry(
+					GetterUtil.getLong(assetListEntryUsage.getKey()));
+
+			if (assetListEntry != null) {
+				mappedContentJSONObject.put(
+					"subtype",
+					_getAssetEntryListSubtypeLabel(
+						assetListEntry, themeDisplay.getLocale())
+				).put(
+					"title", assetListEntry.getTitle()
+				);
+			}
+		}
+
+		if (Objects.equals(
+				assetListEntryUsage.getClassName(),
+				InfoListProvider.class.getName())) {
+
+			InfoListProvider<?> infoListProvider =
+				_infoListProviderTracker.getInfoListProvider(
+					assetListEntryUsage.getKey());
+
+			if (infoListProvider != null) {
+				mappedContentJSONObject.put(
+					"subtype",
+					ResourceActionsUtil.getModelResource(
+						themeDisplay.getLocale(),
+						GenericUtil.getGenericClassName(infoListProvider))
+				).put(
+					"title", infoListProvider.getLabel(themeDisplay.getLocale())
+				);
+			}
+		}
+
+		return mappedContentJSONObject;
+	}
+
+	private String _getSubtypeLabel(
+		AssetListEntry assetListEntry, Locale locale) {
+
+		long classTypeId = GetterUtil.getLong(
+			assetListEntry.getAssetEntrySubtype(), -1);
+
+		if (classTypeId < 0) {
+			return StringPool.BLANK;
+		}
+
+		AssetRendererFactory<?> assetRendererFactory =
+			AssetRendererFactoryRegistryUtil.getAssetRendererFactoryByClassName(
+				assetListEntry.getAssetEntryType());
+
+		if ((assetRendererFactory == null) ||
+			!assetRendererFactory.isSupportsClassTypes()) {
+
+			return StringPool.BLANK;
+		}
+
+		ClassTypeReader classTypeReader =
+			assetRendererFactory.getClassTypeReader();
+
+		try {
+			ClassType classType = classTypeReader.getClassType(
+				classTypeId, locale);
+
+			return classType.getName();
+		}
+		catch (Exception exception) {
+			return StringPool.BLANK;
+		}
+	}
+
+	@Reference
+	private AssetListEntryLocalService _assetListEntryLocalService;
+
+	@Reference
+	private AssetListEntryUsageLocalService _assetListEntryUsageLocalService;
+
+	@Reference
+	private InfoListProviderTracker _infoListProviderTracker;
+
+}
