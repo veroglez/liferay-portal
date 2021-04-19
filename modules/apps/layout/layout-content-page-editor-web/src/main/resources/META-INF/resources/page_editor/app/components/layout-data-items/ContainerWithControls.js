80528fea047e9529fa051fd2f2390d7fb6b62c1b
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2021-04-19 17:46:45 +0200

    LPS-130678 Add visual feedback for a mapped background image in a container

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/layout-data-items/ContainerWithControls.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/layout-data-items/ContainerWithControls.js
@@ -13,7 +13,7 @@
  */
 
 import classNames from 'classnames';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 import useSetRef from '../../../core/hooks/useSetRef';
 import {getLayoutDataItemPropTypes} from '../../../prop-types/index';
@@ -22,13 +22,19 @@ import selectCanUpdateItemConfiguration from '../../selectors/selectCanUpdateIte
 import {useSelector} from '../../store/index';
 import {getFrontendTokenValue} from '../../utils/getFrontendTokenValue';
 import {getResponsiveConfig} from '../../utils/getResponsiveConfig';
+import {useHoveredItemId, useHoveredItemType} from '../Controls';
 import Topper from '../Topper';
 import Container from './Container';
+import isHovered from './isHovered';
 
 const ContainerWithControls = React.forwardRef(({children, item}, ref) => {
 	const canUpdateItemConfiguration = useSelector(
 		selectCanUpdateItemConfiguration
 	);
+	const hoveredItemType = useHoveredItemType();
+	const hoveredItemId = useHoveredItemId();
+	const [hovered, setHovered] = useState(false);
+	const layoutData = useSelector((state) => state.layoutData);
 	const selectedViewportSize = useSelector(
 		(state) => state.selectedViewportSize
 	);
@@ -56,6 +62,21 @@ const ContainerWithControls = React.forwardRef(({children, item}, ref) => {
 	style.minWidth = minWidth;
 	style.width = width;
 
+	useEffect(() => {
+		const backgroundImage =
+			layoutData.items[item.itemId]?.config?.styles?.backgroundImage;
+
+		if (backgroundImage?.classNameId && backgroundImage?.classPK) {
+			setHovered(
+				isHovered({
+					editableValue: backgroundImage,
+					hoveredItemId,
+					hoveredItemType,
+				})
+			);
+		}
+	}, [layoutData.items, item.itemId, hoveredItemId, hoveredItemType]);
+
 	return (
 		<Topper
 			className={classNames({
@@ -66,6 +87,7 @@ const ContainerWithControls = React.forwardRef(({children, item}, ref) => {
 				[`mr-${marginRight}`]:
 					widthType !== CONTAINER_WIDTH_TYPES.fixed,
 				'p-0': widthType === CONTAINER_WIDTH_TYPES.fixed,
+				'page-editor__topper--hovered': hovered,
 			})}
 			item={item}
 			itemElement={itemElement}
