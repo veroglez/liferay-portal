7bb7ec5f6727cdb5573634529aef543bb2acb2a7
Rubén Pulido <ruben.pulido@liferay.com>
2021-06-24 11:26:50 +0200

    LPS-133693 Use numberOfItemsPerPage to compute number of pages

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/layout-data-items/Collection.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/layout-data-items/Collection.js
@@ -184,7 +184,7 @@ const CollectionPagination = ({
 	const isActive = useIsActive();
 
 	const totalPages = Math.ceil(
-		TOTAL_ENTRIES / collectionConfig.numberOfItems
+		TOTAL_ENTRIES / collectionConfig.numberOfItemsPerPage
 	);
 
 	return (
@@ -201,9 +201,10 @@ const CollectionPagination = ({
 						{Liferay.Util.sub(
 							Liferay.Language.get('showing-x-to-x-of-x-entries'),
 							[
-								collectionConfig.numberOfItems *
+								collectionConfig.numberOfItemsPerPage *
 									(activePage - 1) || 1,
-								collectionConfig.numberOfItems * activePage,
+								collectionConfig.numberOfItemsPerPage *
+									activePage,
 								TOTAL_ENTRIES,
 							]
 						)}
