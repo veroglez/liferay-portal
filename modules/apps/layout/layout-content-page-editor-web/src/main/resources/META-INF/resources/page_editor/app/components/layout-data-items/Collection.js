d6bb6dac306171f1f60bbb533c32a5f4feac91fd
Rubén Pulido <ruben.pulido@liferay.com>
2021-06-28 01:30:25 +0200

    LPS-134822 Honor PropsValues.SEARCH_CONTAINER_PAGE_MAX_DELTA when retrieving collection data in edit and view mode

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/layout-data-items/Collection.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/layout-data-items/Collection.js
@@ -176,11 +176,12 @@ const DEFAULT_COLLECTION = {
 };
 
 const CollectionPagination = ({
+	activePage,
 	collectionConfig,
 	collectionId,
+	onPageChange,
 	paginationType,
 }) => {
-	const [activePage, setActivePage] = useState(1);
 	const isActive = useIsActive();
 
 	const totalPages = Math.ceil(
@@ -212,14 +213,14 @@ const CollectionPagination = ({
 
 					<ClayPaginationWithBasicItems
 						activePage={activePage}
-						onPageChange={setActivePage}
+						onPageChange={onPageChange}
 						totalPages={totalPages}
 					/>
 				</ClayPaginationBar>
 			) : (
 				<CollectionSimplePagination
 					activePage={activePage}
-					onChangePage={setActivePage}
+					onChangePage={onPageChange}
 					totalPages={totalPages}
 				/>
 			)}
@@ -228,13 +229,13 @@ const CollectionPagination = ({
 };
 
 const Collection = React.forwardRef(({children, item}, ref) => {
-	const activePage = 1;
 	const child = React.Children.toArray(children)[0];
 	const collectionConfig = item.config;
 
 	const dispatch = useDispatch();
 	const languageId = useSelector(selectLanguageId);
 
+	const [activePage, setActivePage] = useState(1);
 	const [collection, setCollection] = useState(DEFAULT_COLLECTION);
 
 	const context = useContext(CollectionItemContext);
@@ -316,8 +317,10 @@ const Collection = React.forwardRef(({children, item}, ref) => {
 
 			{collectionConfig.paginationType && (
 				<CollectionPagination
+					activePage={activePage}
 					collectionConfig={collectionConfig}
 					collectionId={item.itemId}
+					onPageChange={setActivePage}
 					paginationType={collectionConfig.paginationType}
 				/>
 			)}
