ecaa8aab413b0352e3b4e1c5cb39d02a7b9f2bbb
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2023-02-01 15:57:50 +0100

    LPS-174049 Adds isRestricted property to the PageContentJSONObject

--- modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/util/ContentUtil.java
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/util/ContentUtil.java
@@ -898,6 +898,8 @@ public class ContentUtil {
 				layoutClassedModelUsage.getClassName(),
 				layoutClassedModelUsage.getClassPK())
 		).put(
+			"isRestricted", false
+		).put(
 			"status", _getStatusJSONObject(layoutClassedModelUsage)
 		).put(
 			"subtype",
