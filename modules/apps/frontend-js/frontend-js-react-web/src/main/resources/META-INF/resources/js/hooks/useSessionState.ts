37f95fd6fc5bda3af93c237b45dcb0eecd6132db
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2023-07-03 13:39:24 +0200

    LPS-189184 Move useSessionState to frontend-js-react-web

---
+++ modules/apps/frontend-js/frontend-js-react-web/src/main/resources/META-INF/resources/js/hooks/useSessionState.ts
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import {TYPE_VALUES, sessionStorage} from 'frontend-js-web';
+import {useEffect, useState} from 'react';
+
+/**
+ * Hook to generate a state for a session.
+ */
+
+export default function useSessionState<T>(
+	key: string,
+	defaultValue: T | undefined = undefined,
+	type: TYPE_VALUES = 'CONSENT_TYPE_PERSONALIZATION'
+) {
+	const [state, setState] = useState(() => {
+		const persistedState = sessionStorage.getItem(key, type) || '';
+
+		try {
+			const deserializedValue = JSON.parse(persistedState);
+
+			if (
+				!(deserializedValue === null || deserializedValue === undefined)
+			) {
+				return deserializedValue as T;
+			}
+		}
+		catch (_error) {}
+
+		return defaultValue;
+	});
+
+	useEffect(() => {
+		if (state === null || state === undefined) {
+			sessionStorage.removeItem(key);
+		}
+		else {
+			sessionStorage.setItem(key, JSON.stringify(state), type);
+		}
+	}, [key, state, type]);
+
+	return [state, setState] as const;
+}
