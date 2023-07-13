0323ec33b284a70a11bb39a34d72fbfc41d533ee
mikellorza <mikel.lorza@liferay.com>
2023-07-13 18:51:54 +0200

    LPS-190367 Unbind product navigation icon from the layoutReportsGooglePageSpeed configuration if the FF is enabled

--- modules/apps/layout/layout-reports-web/src/main/java/com/liferay/layout/reports/web/internal/product/navigation/control/menu/LayoutReportsProductNavigationControlMenuEntry.java
+++ modules/apps/layout/layout-reports-web/src/main/java/com/liferay/layout/reports/web/internal/product/navigation/control/menu/LayoutReportsProductNavigationControlMenuEntry.java
@@ -167,9 +167,10 @@ public class LayoutReportsProductNavigationControlMenuEntry
 
 		long scopeGroupId = _portal.getScopeGroupId(httpServletRequest);
 
-		if ((scopeGroupId == 0) ||
-			!_layoutReportsGooglePageSpeedConfigurationProvider.isEnabled(
-				_groupLocalService.getGroup(scopeGroupId))) {
+		if (!FeatureFlagManagerUtil.isEnabled("LPS-187284") &&
+			((scopeGroupId == 0) ||
+			 !_layoutReportsGooglePageSpeedConfigurationProvider.isEnabled(
+				 _groupLocalService.getGroup(scopeGroupId)))) {
 
 			return false;
 		}
@@ -177,6 +178,17 @@ public class LayoutReportsProductNavigationControlMenuEntry
 		ThemeDisplay themeDisplay =
 			(ThemeDisplay)httpServletRequest.getAttribute(
 				WebKeys.THEME_DISPLAY);
+
+		Layout layout = themeDisplay.getLayout();
+
+		if (FeatureFlagManagerUtil.isEnabled("LPS-187284") &&
+			((scopeGroupId == 0) ||
+			 (!_layoutReportsGooglePageSpeedConfigurationProvider.isEnabled(
+				 _groupLocalService.getGroup(scopeGroupId)) &&
+			  !layout.isTypeContent()))) {
+
+			return false;
+		}
 
 		if (!_isShow(themeDisplay) || !_isShowPanel(httpServletRequest)) {
 			return false;
