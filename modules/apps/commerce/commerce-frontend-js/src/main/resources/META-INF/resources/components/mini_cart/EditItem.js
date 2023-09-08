7b28799c9574bb197264d4a164cf4ec5dc986130
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2023-09-08 19:32:22 +0200

    COMMERCE-12235 Add selectors with options

--- modules/apps/commerce/commerce-frontend-js/src/main/resources/META-INF/resources/components/mini_cart/EditItem.js
+++ modules/apps/commerce/commerce-frontend-js/src/main/resources/META-INF/resources/components/mini_cart/EditItem.js
@@ -4,24 +4,60 @@
  */
 
 import ClayButton, {ClayButtonWithIcon} from '@clayui/button';
-import {sub} from 'frontend-js-web';
-import React, {useContext} from 'react';
+import ClayForm, {ClaySelect} from '@clayui/form';
+import ClayIcon from '@clayui/icon';
+import {fetch, sub} from 'frontend-js-web';
+import React, {useContext, useEffect, useMemo, useState} from 'react';
 
+import {CHANNEL_RESOURCE_ENDPOINT} from '../../utilities/constants';
 import MiniCartContext from './MiniCartContext';
 
+const getProductOptions = (channelId, productId) => {
+	const url = new URL(
+		`${themeDisplay.getPathContext()}${CHANNEL_RESOURCE_ENDPOINT}/${channelId}/products/${productId}/product-options`,
+		themeDisplay.getPortalURL()
+	);
+
+	return url.toString();
+};
+
 function EditItem() {
-	const {editedItem, setEditedItem} = useContext(MiniCartContext);
+	const [options, setOptions] = useState([]);
+	const {
+		cartState: {
+			cartItems,
+			channel: {channel},
+		},
+		editedItem,
+		setEditedItem,
+	} = useContext(MiniCartContext);
+
 	const backLabel = sub(
 		Liferay.Language.get('go-to-x'),
 		Liferay.Language.get('products')
 	);
 
+	const selectedItem = useMemo(
+		() => cartItems.find((item) => item.productId === editedItem.productId),
+		[cartItems, editedItem]
+	);
+
+	useEffect(() => {
+		const url = getProductOptions(channel.id, editedItem.productId);
+
+		fetch(url)
+			.then((response) => response.json())
+			.then((data) => setOptions(data))
+			.catch((error) => console.error(error));
+	}, [channel.id, editedItem.productId]);
+
 	return (
 		<>
 			<div className="d-flex flex-column h-100 mini-cart-edit-item">
-				<div className="align-items-center d-flex mini-cart-header px-4 py-1">
+				<div className="align-items-center d-flex mini-cart-header px-4">
 					<ClayButtonWithIcon
 						aria-label={backLabel}
+						className="d-inline-flex"
 						displayType="unstyled"
 						onClick={() => setEditedItem(null)}
 						symbol="angle-left"
@@ -33,9 +69,18 @@ function EditItem() {
 					</span>
 				</div>
 
-				<div className="flex-grow-1 p-4"></div>
+				<div className="flex-grow-1 p-4">
+					{options?.items?.length > 0 ? (
+						<ClayForm>
+							<Options
+								items={options.items}
+								selectedItem={selectedItem}
+							/>
+						</ClayForm>
+					) : null}
+				</div>
 
-				<div className="mini-cart-footer p-4 text-right">
+				<div className="mini-cart-footer px-4 py-2 text-right">
 					<ClayButton
 						className="mr-3"
 						displayType="secondary"
@@ -52,3 +97,43 @@ function EditItem() {
 }
 
 export default EditItem;
+
+const Options = ({items, selectedItem}) => {
+	const [selectedOptions, setSelectedOptions] = useState(() =>
+		JSON.parse(selectedItem.options).reduce(
+			(acc, {key, value}) => ({...acc, [key]: value}),
+			{}
+		)
+	);
+
+	return items.map(({id, key, name, productOptionValues}) => (
+		<ClayForm.Group key={id}>
+			<label htmlFor={`${id}-${name}`}>
+				{name}
+
+				<span className="reference-mark">
+					<ClayIcon symbol="asterisk" />
+				</span>
+			</label>
+
+			<ClaySelect
+				id={`${id}-${name}`}
+				onChange={({target: {value}}) =>
+					setSelectedOptions((selectedOptions) => ({
+						...selectedOptions,
+						[key]: value,
+					}))
+				}
+			>
+				{productOptionValues.map((item) => (
+					<ClaySelect.Option
+						key={item.id}
+						label={item.name}
+						selected={selectedOptions[key] === item.key}
+						value={item.key}
+					/>
+				))}
+			</ClaySelect>
+		</ClayForm.Group>
+	));
+};
