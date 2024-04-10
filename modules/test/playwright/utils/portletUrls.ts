25b0b336882aa93d8b040c5d68f628352c31d947
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2024-04-10 09:03:26 +0200

    LPD-22782 Add collection admin page to PORTLET_URLS

--- modules/test/playwright/utils/portletUrls.ts
+++ modules/test/playwright/utils/portletUrls.ts
@@ -3,6 +3,8 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 export const PORTLET_URLS = {
+	collections:
+		'/~/control_panel/manage?p_p_id=com_liferay_asset_list_web_portlet_AssetListPortlet',
 	displayPageTemplates:
 		'/~/control_panel/manage?p_p_id=com_liferay_layout_page_template_admin_web_portlet_LayoutPageTemplatesPortlet&_com_liferay_layout_page_template_admin_web_portlet_LayoutPageTemplatesPortlet_tabs1=display-page-templates',
 	documentLibrary:
