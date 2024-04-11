549aada4c44e05e62604f3f096a89b0bc0708257
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2024-04-11 17:05:38 +0200

    LPD-22782 Add CollectionPage with methods related to collections

---
+++ modules/test/playwright/pages/asset-list-web/CollectionPage.ts
@@ -0,0 +1,88 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import {Page} from '@playwright/test';
+
+import {PORTLET_URLS} from '../../utils/portletUrls';
+
+export class CollectionPage {
+	readonly page: Page;
+
+	constructor(page: Page) {
+		this.page = page;
+	}
+
+	async goto(siteUrl?: Site['friendlyUrlPath']) {
+		await this.page.goto(
+			`/group${siteUrl || '/guest'}${PORTLET_URLS.collections}`
+		);
+	}
+
+	/**
+	 * Creates a dynamic collection with the given name.
+	 */
+
+	async createWebContentDynamicCollection(name, siteUrl) {
+		await this.addNewDynamicCollection(name);
+
+		await this.configureCollectionWithWebContents();
+
+		await this.page.getByRole('button', {name: 'Save'}).click();
+
+		await this.page.waitForTimeout(3000);
+
+		return {
+			collectionId: await this.getCollectionClassPK(name, siteUrl),
+		};
+	}
+
+	/**
+	 * Add a dynamic collection with the given name.
+	 */
+
+	async addNewDynamicCollection(name) {
+		await this.page.getByRole('button', {name: 'New'}).first().click();
+
+		await this.page
+			.getByRole('menuitem', {name: 'Dynamic Collection'})
+			.click();
+
+		await this.page.getByPlaceholder('Title').fill(name);
+
+		await this.page.getByRole('button', {name: 'Save'}).click();
+
+		await this.page.waitForTimeout(3000);
+	}
+
+	/**
+	 * Configure a dynamic collection for Web Contents.
+	 */
+
+	async configureCollectionWithWebContents() {
+		await this.page
+			.getByLabel('Item Type')
+			.selectOption({label: 'Web Content Article'});
+
+		await this.page.waitForTimeout(3000);
+
+		await this.page
+			.locator('.asset-subtype:not(.hide)')
+			.getByLabel('Item Subtype')
+			.selectOption({label: 'Basic Web Content'});
+	}
+
+	/**
+	 * Gets the collection classPK.
+	 */
+
+	async getCollectionClassPK(name, siteUrl) {
+		await this.goto(siteUrl);
+
+		return await this.page
+			.locator(`button[data-assetlistentrytitle="${name}"]`)
+			.first()
+			.evaluate((event) => event.dataset.assetlistentryid);
+	}
+}
