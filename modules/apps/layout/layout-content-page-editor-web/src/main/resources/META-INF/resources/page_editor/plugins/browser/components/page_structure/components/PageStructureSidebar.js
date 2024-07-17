c4bac684166458d107dd3f086e1ddcf329eb3450
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2024-07-17 16:49:54 +0200

    LPD-30905 Add top bar with the selected items in browser panel

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/plugins/browser/components/page_structure/components/PageStructureSidebar.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/plugins/browser/components/page_structure/components/PageStructureSidebar.js
@@ -3,16 +3,27 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import classNames from 'classnames';
+import {ManagementToolbar} from 'frontend-js-components-web';
+import {sub} from 'frontend-js-web';
 import React from 'react';
 
+import {useActiveItemIds} from '../../../../../app/contexts/ControlsContext';
 import StructureTree from './StructureTree';
 
 export default function PageStructureSidebar() {
+	const activeItemIds = useActiveItemIds();
+
 	return (
-		<div
-			className={classNames('overflow-auto page-editor__page-structure')}
-		>
+		<div className="overflow-auto page-editor__page-structure">
+			{Liferay.FeatureFlags['LPD-18221'] && activeItemIds.length > 1 ? (
+				<ManagementToolbar.Container active>
+					{sub(
+						Liferay.Language.get('x-items-selected'),
+						activeItemIds.length
+					)}
+				</ManagementToolbar.Container>
+			) : null}
+
 			<StructureTree />
 		</div>
 	);
