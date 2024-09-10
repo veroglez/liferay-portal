16bd8911a04bb4b473da0d2ec59b9616063fcd00
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2024-09-10 14:16:00 +0200

    LPD-30906 Migrate moveItem to typescript

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/thunks/moveItem.ts
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/thunks/moveItem.ts
@@ -3,11 +3,24 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+import {State} from '../../types/State';
 import moveItemAction from '../actions/moveItem';
+import updateNetwork from '../actions/updateNetwork';
 import LayoutService from '../services/LayoutService';
 
-export default function moveItem({itemIds, parentItemIds, positions}) {
-	return (dispatch, getState) => {
+type Props = {
+	itemIds: string[];
+	parentItemIds: string[];
+	positions: number[];
+};
+
+export default function moveItem({itemIds, parentItemIds, positions}: Props) {
+	return (
+		dispatch: (
+			action: ReturnType<typeof updateNetwork | typeof moveItemAction>
+		) => void,
+		getState: () => State
+	) => {
 		return LayoutService.moveItem({
 			itemIds,
 			onNetworkStatus: dispatch,
