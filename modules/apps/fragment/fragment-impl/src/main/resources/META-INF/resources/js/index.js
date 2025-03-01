a0dea09a10d25d9cd6578e3c18776d48b6367998
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2025-03-01 16:32:48 +0100

    LPD-49713 Remove registerLocalizedFileInput

--- modules/apps/fragment/fragment-impl/src/main/resources/META-INF/resources/js/index.js
+++ modules/apps/fragment/fragment-impl/src/main/resources/META-INF/resources/js/index.js
@@ -4,6 +4,5 @@
  */
 
 export {LocalizationSelect} from './LocalizationSelect';
-export {registerLocalizedFileInput} from './registerLocalizedFileInput';
 export {registerLocalizedInput} from './registerLocalizedInput';
 export {registerUnlocalizedInput} from './registerUnlocalizedInput';
