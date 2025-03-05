6717bbda24ad1684a5c679dc863e49d0561b90af
Georgel Pop <georgel.pop@liferay.com>
2025-03-05 19:18:38 +0100

    LPD-49736 Call MarketplaceSearchResults.tsx component inside SearchResultsPanel.js

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/plugins/fragments_and_widgets/components/SearchResultsPanel.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/plugins/fragments_and_widgets/components/SearchResultsPanel.js
@@ -8,51 +8,69 @@ import ClayLoadingIndicator from '@clayui/loading-indicator';
 import PropTypes from 'prop-types';
 import React from 'react';
 
+import {useSelector} from '../../../app/contexts/StoreContext';
+import MarketplaceSearchResults from './MarketplaceSearchResults';
 import TabCollection from './TabCollection';
 
-export default function SearchResultsPanel({filteredTabs, loading = false}) {
+export default function SearchResultsPanel({
+	filteredTabs,
+	loading = false,
+	searchValue,
+}) {
+	const permissions = useSelector((state) => state.permissions);
+
 	if (loading) {
 		return <ClayLoadingIndicator className="mt-3" size="sm" />;
 	}
 
-	return filteredTabs.length ? (
-		<div className="overflow-auto px-3">
-			{filteredTabs.map((tab, index) => (
-				<div key={index}>
-					<div className="font-weight-semi-bold page-editor__fragments-widgets__search-results-panel__filter-subtitle py-2">
-						{tab.label}
-					</div>
+	return (
+		<div className="overflow-auto">
+			{filteredTabs.length ? (
+				<div>
+					{filteredTabs.map((tab, index) => (
+						<div key={index}>
+							<div className="font-weight-semi-bold page-editor__fragments-widgets__search-results-panel__filter-subtitle px-3 py-2">
+								{tab.label}
+							</div>
 
-					<ul
-						aria-orientation="vertical"
-						className="list-unstyled panel-group-sm"
-						role="menubar"
-					>
-						{tab.collections.map((collection, index) => (
-							<TabCollection
-								collection={collection}
-								initialOpen
-								isSearchResult
-								key={index}
-							/>
-						))}
-					</ul>
+							<ul
+								aria-orientation="vertical"
+								className="list-unstyled panel-group-sm px-3"
+								role="menubar"
+							>
+								{tab.collections.map((collection, index) => (
+									<TabCollection
+										collection={collection}
+										initialOpen
+										isSearchResult
+										key={index}
+									/>
+								))}
+							</ul>
+						</div>
+					))}
 				</div>
-			))}
-		</div>
-	) : (
-		<ClayEmptyState
-			description={Liferay.Language.get(
-				'try-again-with-a-different-search'
+			) : (
+				<ClayEmptyState
+					description={Liferay.Language.get(
+						'try-again-with-a-different-search'
+					)}
+					imgSrc={`${themeDisplay.getPathThemeImages()}/states/search_state.svg`}
+					small
+					title={Liferay.Language.get('no-results-found')}
+				/>
 			)}
-			imgSrc={`${themeDisplay.getPathThemeImages()}/states/search_state.svg`}
-			small
-			title={Liferay.Language.get('no-results-found')}
-		/>
+
+			{Liferay.FeatureFlags['LPD-34938'] &&
+			permissions.VIEW_MARKETPLACE ? (
+				<MarketplaceSearchResults searchValue={searchValue} />
+			) : null}
+		</div>
 	);
 }
 
 SearchResultsPanel.proptypes = {
 	filteredTabs: PropTypes.object.isRequired,
 	loading: PropTypes.bool,
+	searchValue: PropTypes.string.isRequired,
 };
