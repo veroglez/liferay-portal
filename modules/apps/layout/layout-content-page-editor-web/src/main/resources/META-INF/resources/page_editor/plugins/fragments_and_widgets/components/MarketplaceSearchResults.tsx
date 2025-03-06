5066d4b596cd75f8a252904b561c1e159600a65a
Georgel Pop <georgel.pop@liferay.com>
2025-03-06 20:50:34 +0100

    LPD-50762 Add SearchResultsMessage component to announce results number

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/plugins/fragments_and_widgets/components/MarketplaceSearchResults.tsx
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/plugins/fragments_and_widgets/components/MarketplaceSearchResults.tsx
@@ -6,7 +6,10 @@
 import ClayButton from '@clayui/button';
 import ClayEmptyState from '@clayui/empty-state';
 import ClayLoadingIndicator from '@clayui/loading-indicator';
-import {MarketplaceModal} from '@liferay/layout-js-components-web';
+import {
+	MarketplaceModal,
+	SearchResultsMessage,
+} from '@liferay/layout-js-components-web';
 import {
 	APIResponse,
 	MarketplaceRest,
@@ -128,6 +131,10 @@ function SearchResultsPanel({searchValue}: {searchValue: string}) {
 
 	return (
 		<>
+			{results && !loading && (
+				<SearchResultsMessage numberOfResults={results.items.length} />
+			)}
+
 			<p className="pb-2 pl-3 pt-3 text-3 text-secondary">
 				{Liferay.Language.get('showing-results-from-marketplace')}
 			</p>
