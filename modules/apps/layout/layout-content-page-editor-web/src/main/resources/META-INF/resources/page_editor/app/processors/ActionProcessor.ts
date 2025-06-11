ccb74aa020e072fe4cf104bd1b08858822e3c61e
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2025-06-11 16:41:08 +0200

    LPD-57530 Migrate ActionProcessor to typescript

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/processors/ActionProcessor.ts
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/processors/ActionProcessor.ts
@@ -3,5 +3,6 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import getAlloyEditorProcessor from './getAlloyEditorProcessor';
+import {getAlloyEditorProcessor} from '../js-index';
+
 export default getAlloyEditorProcessor('text');
