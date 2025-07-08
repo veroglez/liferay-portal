c0e4c0b50c847085a28d318c312df578389df794
Victor Galan <victor.galan@liferay.com>
2025-07-08 15:43:59 +0200

    LPD-59992 Inject sidebar information

--- modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/fragment/renderer/ContentEditorSidePanelComponentSectionFragmentRenderer.java
+++ modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/fragment/renderer/ContentEditorSidePanelComponentSectionFragmentRenderer.java
@@ -12,13 +12,23 @@ import com.liferay.layout.display.page.constants.LayoutDisplayPageWebKeys;
 import com.liferay.object.model.ObjectDefinition;
 import com.liferay.object.model.ObjectEntry;
 import com.liferay.object.service.ObjectDefinitionLocalService;
+import com.liferay.portal.kernel.comment.Comment;
+import com.liferay.portal.kernel.comment.CommentManager;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactory;
+import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.site.cms.site.initializer.internal.util.CommentUtil;
+import com.liferay.subscription.service.SubscriptionLocalService;
 
 import jakarta.servlet.http.HttpServletRequest;
 
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
 import org.osgi.service.component.annotations.Component;
@@ -69,7 +79,62 @@ public class ContentEditorSidePanelComponentSectionFragmentRenderer
 		ObjectEntry objectEntry = (ObjectEntry)displayObject;
 
 		return HashMapBuilder.<String, Object>put(
+			"comments",
+			() -> {
+				JSONArray jsonArray = _jsonFactory.createJSONArray();
+
+				if (!_commentManager.hasDiscussion(
+						objectEntry.getModelClassName(),
+						objectEntry.getObjectEntryId())) {
+
+					return jsonArray;
+				}
+
+				List<Comment> rootComments = _commentManager.getRootComments(
+					objectEntry.getModelClassName(),
+					objectEntry.getObjectEntryId(),
+					WorkflowConstants.STATUS_ANY, QueryUtil.ALL_POS,
+					QueryUtil.ALL_POS);
+
+				for (Comment rootComment : rootComments) {
+					JSONObject commentJSONObject =
+						CommentUtil.getCommentJSONObject(
+							rootComment, httpServletRequest);
+
+					List<Comment> childComments =
+						_commentManager.getChildComments(
+							rootComment.getCommentId(),
+							WorkflowConstants.STATUS_APPROVED,
+							QueryUtil.ALL_POS, QueryUtil.ALL_POS);
+
+					JSONArray childCommentsJSONArray =
+						_jsonFactory.createJSONArray();
+
+					for (Comment childComment : childComments) {
+						childCommentsJSONArray.put(
+							CommentUtil.getCommentJSONObject(
+								childComment, httpServletRequest));
+					}
+
+					commentJSONObject.put("children", childCommentsJSONArray);
+
+					jsonArray.put(commentJSONObject);
+				}
+
+				return jsonArray;
+			}
+		).put(
 			"id", String.valueOf(objectEntry.getObjectEntryId())
+		).put(
+			"isSubscribed",
+			() -> {
+				ThemeDisplay themeDisplay = (ThemeDisplay)httpServletRequest;
+
+				return _subscriptionLocalService.isSubscribed(
+					themeDisplay.getCompanyId(), themeDisplay.getUserId(),
+					objectEntry.getModelClassName(),
+					objectEntry.getObjectEntryId());
+			}
 		).put(
 			"type",
 			() -> {
@@ -89,6 +154,15 @@ public class ContentEditorSidePanelComponentSectionFragmentRenderer
 	}
 
 	@Reference
+	private CommentManager _commentManager;
+
+	@Reference
+	private JSONFactory _jsonFactory;
+
+	@Reference
 	private ObjectDefinitionLocalService _objectDefinitionLocalService;
+
+	@Reference
+	private SubscriptionLocalService _subscriptionLocalService;
 
 }
