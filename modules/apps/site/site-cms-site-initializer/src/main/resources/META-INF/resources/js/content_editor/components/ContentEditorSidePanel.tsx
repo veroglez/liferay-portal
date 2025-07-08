07444e8eeb0df5ef285c388fb1e7e8930ec65c2c
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2025-07-08 14:35:30 +0200

    LPD-59851 Move GeneralPanel component to panels

--- modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/content_editor/components/ContentEditorSidePanel.tsx
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/content_editor/components/ContentEditorSidePanel.tsx
@@ -7,9 +7,9 @@ import '../../../css/content_editor/ContentEditorSidePanel.scss';
 
 import {Button, VerticalBar} from '@clayui/core';
 import ClayIcon from '@clayui/icon';
-import ClayLabel from '@clayui/label';
-import {sub} from 'frontend-js-web';
 import React from 'react';
+
+import GeneralPanel from './panels/GeneralPanel';
 
 type Props = {
 	id: string;
@@ -63,41 +63,5 @@ export default function ContentEditorSidePanel(props: Props) {
 				)}
 			</VerticalBar.Bar>
 		</VerticalBar>
-	);
-}
-
-function GeneralPanel({id, type, version}: Props) {
-	const Row = ({
-		children,
-		label,
-	}: {
-		children: React.ReactNode;
-		label: string;
-	}) => (
-		<div className="border-bottom d-flex justify-content-between mb-2 pb-2">
-			<span className="font-weight-semi-bold text-secondary">
-				{label}
-			</span>
-
-			{children}
-		</div>
-	);
-
-	return (
-		<div className="p-3">
-			<Row label={Liferay.Language.get('type')}>
-				<span>{type}</span>
-			</Row>
-
-			<Row label={Liferay.Language.get('version')}>
-				<ClayLabel displayType="info">
-					{sub(Liferay.Language.get('version-x'), version)}
-				</ClayLabel>
-			</Row>
-
-			<Row label={Liferay.Language.get('id')}>
-				<span>{id}</span>
-			</Row>
-		</div>
 	);
 }
