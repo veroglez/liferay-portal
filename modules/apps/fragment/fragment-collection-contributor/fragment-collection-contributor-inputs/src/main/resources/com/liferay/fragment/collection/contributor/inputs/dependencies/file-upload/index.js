542e8cd773d80d1ae58724bf78e566685586b100
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2025-08-22 15:54:19 +0200

    LPD-x Improve readability of the select file button

--- modules/apps/fragment/fragment-collection-contributor/fragment-collection-contributor-inputs/src/main/resources/com/liferay/fragment/collection/contributor/inputs/dependencies/file-upload/index.js
+++ modules/apps/fragment/fragment-collection-contributor/fragment-collection-contributor-inputs/src/main/resources/com/liferay/fragment/collection/contributor/inputs/dependencies/file-upload/index.js
@@ -3,10 +3,8 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-const wrapper = fragmentElement;
-
 const fileInput = document.getElementById(`${fragmentNamespace}-file-upload`);
-const fileName = wrapper.querySelector('.forms-file-upload-file-name');
+const fileName = document.getElementById(`${fragmentNamespace}-file-name`);
 const hiddenFileInput = document.getElementById(
 	`${fragmentNamespace}-file-upload-hidden`
 );
