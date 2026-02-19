c829c81eeac83a0637c6e9b5805d4d47267f4f8b
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2026-02-19 13:44:24 +0100

    LPD-70107 The icons in the home cards are currently removed until they are available

--- modules/apps/product-navigation/product-navigation-applications-menu-web/src/main/resources/META-INF/resources/js/homes/components/CategoryCardHorizontal.tsx
+++ modules/apps/product-navigation/product-navigation-applications-menu-web/src/main/resources/META-INF/resources/js/homes/components/CategoryCardHorizontal.tsx
@@ -25,16 +25,18 @@ const CategoryCardHorizontal = ({
 		>
 			<ClayCard.Body className="c-p-1 c-p-lg-2">
 				<ClayCard.Row>
-					<ClayLayout.ContentCol>
-						<ClaySticker
-							className="border-0"
-							displayType="outline"
-							inline
-							size="sm"
-						>
-							<ClayIcon fontSize={12} symbol={leadingIcon} />
-						</ClaySticker>
-					</ClayLayout.ContentCol>
+					{leadingIcon ? (
+						<ClayLayout.ContentCol>
+							<ClaySticker
+								className="border-0"
+								displayType="outline"
+								inline
+								size="sm"
+							>
+								<ClayIcon fontSize={12} symbol={leadingIcon} />
+							</ClaySticker>
+						</ClayLayout.ContentCol>
+					) : null}
 
 					<ClayLayout.ContentCol expand>
 						<ClayCard.Description
