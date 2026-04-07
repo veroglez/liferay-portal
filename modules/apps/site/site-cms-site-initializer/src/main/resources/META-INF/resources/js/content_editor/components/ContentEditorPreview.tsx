a4e9f30ed79e81ca4da6761fb648ac6c0e7d299d
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2026-04-07 17:55:44 +0200

    LPD-84613 Use PanelResizer to resize the preview

--- modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/content_editor/components/ContentEditorPreview.tsx
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/content_editor/components/ContentEditorPreview.tsx
@@ -3,16 +3,29 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+import {PanelResizer as Resizer, useElementMaxWidth} from '@clayui/shared';
+import {useEventListener} from '@liferay/frontend-js-react-web';
 import classNames from 'classnames';
 import {sub} from 'frontend-js-web';
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 
 import {EVENT_HANDLE_PREVIEW} from './ContentEditorToolbar';
 
 import '../../../css/content_editor/ContentEditorPreview.scss';
 
+const PREVIEW_WIDTH_MIN = 500;
+
 export default function ContentEditorPreview({title}: {title: string}) {
 	const [isVisible, setIsVisible] = useState<boolean>(false);
+	const [resizeWidth, setResizeWidth] = useState<number>(
+		window.innerWidth / 2
+	);
+	const [resizing, setResizing] = useState<boolean>(false);
+
+	const previewRef = useRef<HTMLDivElement>(null);
+
+	const previewWidthMax = useElementMaxWidth(previewRef);
+	const previewWidth = Math.min(previewWidthMax, resizeWidth);
 
 	useEffect(() => {
 		const handlePreview = ({showPreview}: {showPreview: boolean}) => {
@@ -24,15 +37,20 @@ export default function ContentEditorPreview({title}: {title: string}) {
 		return () => Liferay.detach(EVENT_HANDLE_PREVIEW, handlePreview);
 	}, []);
 
+	useEventListener('mouseup', () => setResizing(false), true, document);
+
 	if (!Liferay.FeatureFlags['LPD-44507']) {
 		return null;
 	}
 
 	return (
 		<div
-			className={classNames('content-editor__preview', {
+			className={classNames('content-editor__preview c-slideout-end', {
+				resizing,
 				visible: isVisible,
 			})}
+			ref={previewRef}
+			style={{width: previewWidth}}
 		>
 			{isVisible ? (
 				<div className="border-bottom d-flex justify-content-between p-3">
@@ -41,6 +59,17 @@ export default function ContentEditorPreview({title}: {title: string}) {
 					</span>
 				</div>
 			) : null}
+
+			<Resizer
+				onPanelWidthChange={(width) => {
+					setResizeWidth(width);
+					setResizing(true);
+				}}
+				panelWidth={previewWidth}
+				panelWidthMax={previewWidthMax}
+				panelWidthMin={PREVIEW_WIDTH_MIN}
+				position="right"
+			/>
 		</div>
 	);
 }
