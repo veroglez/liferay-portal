4d93f354c48c152f7ba88152857c87a58af74843
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2026-04-13 18:43:25 +0200

    LPD-85584 Isolate the preview header

--- modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/content_editor/components/ContentEditorPreview.tsx
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/content_editor/components/ContentEditorPreview.tsx
@@ -3,19 +3,17 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {ClayButtonWithIcon} from '@clayui/button';
 import {PanelResizer as Resizer, useObservedMaxWidth} from '@clayui/shared';
 import {useEventListener} from '@liferay/frontend-js-react-web';
-import {PopoverTooltip} from '@liferay/layout-js-components-web';
 import classNames from 'classnames';
 import {useSessionState} from 'frontend-js-components-web';
-import {sub} from 'frontend-js-web';
 import React, {useCallback, useEffect, useId, useRef, useState} from 'react';
 
 import {
 	EVENT_CLOSE_PREVIEW,
 	EVENT_HANDLE_PREVIEW,
 } from './ContentEditorToolbar';
+import PreviewHeader from './preview/PreviewHeader';
 
 import '../../../css/content_editor/ContentEditorPreview.scss';
 
@@ -107,52 +105,15 @@ export default function ContentEditorPreview({title}: {title: string}) {
 			tabIndex={-1}
 		>
 			{isVisible ? (
-				<div className="border-bottom d-flex justify-content-between p-3">
-					<span className="font-weight-bold text-6" id={titleId}>
-						{sub(Liferay.Language.get('x-preview'), title)}
+				<PreviewHeader
+					onClosePreview={() => {
+						Liferay.fire(EVENT_CLOSE_PREVIEW);
 
-						<PopoverTooltip
-							alignPosition="bottom"
-							content={Liferay.Language.get(
-								'save-as-draft-or-publish-content-to-see-your-latest-changes-in-the-preview'
-							)}
-							header={Liferay.Language.get('preview-help')}
-							trigger={
-								<ClayButtonWithIcon
-									aria-description={Liferay.Language.get(
-										'save-as-draft-or-publish-content-to-see-your-latest-changes-in-the-preview'
-									)}
-									aria-label={Liferay.Language.get(
-										'preview-help'
-									)}
-									borderless
-									className="ml-1 text-secondary"
-									displayType="unstyled"
-									monospaced
-									size="sm"
-									symbol="question-circle"
-								/>
-							}
-						/>
-					</span>
-
-					<ClayButtonWithIcon
-						aria-label={sub(
-							Liferay.Language.get('close-x'),
-							Liferay.Language.get('preview')
-						)}
-						borderless
-						displayType="secondary"
-						monospaced
-						onClick={() => {
-							Liferay.fire(EVENT_CLOSE_PREVIEW);
-
-							setIsVisible(false);
-						}}
-						size="sm"
-						symbol="times"
-					/>
-				</div>
+						setIsVisible(false);
+					}}
+					title={title}
+					titleId={titleId}
+				/>
 			) : null}
 
 			<Resizer
