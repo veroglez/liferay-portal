55eafff3e8b0dfb6c8c453f233974b3c5ad46b76
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2026-04-22 18:36:49 +0200

    LPD-87037 Take into account the translation language in the preview URL

--- modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/content_editor/components/preview/usePreviewState.ts
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/content_editor/components/preview/usePreviewState.ts
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import React, {useCallback, useMemo, useState} from 'react';
+import React, {useCallback, useEffect, useMemo, useState} from 'react';
 
 import ApiHelper from '../../../common/services/ApiHelper';
 import {useCache} from '../../../structure_builder/contexts/CacheContext';
@@ -20,9 +20,28 @@ export type Site = {
 };
 
 export default function usePreviewState(getPreviewDataURL: string) {
+	const [languageId, setLanguageId] = useState<string>(
+		Liferay.ThemeDisplay.getDefaultLanguageId()
+	);
 	const [selectedChannelKey, setSelectedChannelKey] = useState<React.Key>('');
 	const [selectedDisplayPageKey, setSelectedDisplayPageKey] =
 		useState<React.Key>('');
+
+	useEffect(() => {
+		const handleLocaleChanged = ({
+			languageId,
+		}: {
+			languageId: Liferay.Language.Locale;
+		}) => setLanguageId(languageId);
+
+		Liferay.on('localizationSelect:localeChanged', handleLocaleChanged);
+
+		return () =>
+			Liferay.detach(
+				'localizationSelect:localeChanged',
+				handleLocaleChanged
+			);
+	}, []);
 
 	const getSites = useCallback(async () => {
 		const {data, error} = await ApiHelper.get<Site[]>(getPreviewDataURL);
@@ -63,13 +82,24 @@ export default function usePreviewState(getPreviewDataURL: string) {
 		[selectedChannelKey, sites]
 	);
 
-	const previewURL = useMemo(
-		() =>
-			displayPageTemplates?.find(
-				({plid}) => plid === selectedDisplayPageKey
-			)?.url,
-		[displayPageTemplates, selectedDisplayPageKey]
-	);
+	const previewURL = useMemo(() => {
+		const url = displayPageTemplates?.find(
+			({plid}) => plid === selectedDisplayPageKey
+		)?.url;
+
+		if (
+			!url ||
+			languageId === Liferay.ThemeDisplay.getDefaultLanguageId()
+		) {
+			return url;
+		}
+
+		const localizedURL = new URL(url, window.location.origin);
+
+		localizedURL.searchParams.set('languageId', languageId);
+
+		return localizedURL.toString();
+	}, [displayPageTemplates, languageId, selectedDisplayPageKey]);
 
 	const selectChannel = useCallback((key: React.Key) => {
 		setSelectedChannelKey(key);
